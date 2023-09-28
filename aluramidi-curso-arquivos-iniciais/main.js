c5d5ff29891cd637d13e3b9b226f51b37f7072c1
stephannyjs <115154320+stephannyjs@users.noreply.github.com>
2023-09-28 08:55:53 -0300

    Add files via upload

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,1 +1,18 @@
-document.querySelector('.tecla_pom')
+// Pom ---------------------------------------- 
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+ const listaDeTeclas = document.querySelectorAll('.tecla');
+
+ listaDeTeclas[0].onclik = tocaSomPom
+
+ //enquanto
+ while (contador < listaDeTeclas.length){
+  
+    listaDeTeclas[contador].onclik = tocaSomPom
+
+contador = contador + 1;
+
+console.log(contador)
+ }
