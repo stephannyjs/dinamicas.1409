eca56cadccc4ef946921a24827b0cf732467aa10
stephannyjs <115154320+stephannyjs@users.noreply.github.com>
2023-10-19 08:48:48 -0300

    Add files via upload

--- dinamicas.1409-main/aluramidi-curso-arquivos-iniciais/main.js
+++ dinamicas.1409-main/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,31 +1,36 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSomPom (selectorAudio) {
+  const elemento = document.querySelector(selectorAudio).play();
+
+    if (elemento && elemento.localName === 'audio') {
+           elemento.play();
+        }
+        else {
+            //alert('Elemento não encontrado');
+            console.log('elemento não encontrado ou seletor inválido')
+        }
 }
 
  const listaDeTeclas = document.querySelectorAll('.tecla');
 
  //para
- for (let contador = 0; contador < listaDeTeclas.length; contador++){
-
+ for(let contador = 0; contador < listaDeTeclas.length; contador++);
+ 
     const tecla = listaDeTeclas[contador];
-    const instrumento listaDeTeclas[contador].classList[1];
-    const idAudio = '#som_${instrumento}';
-
-    tecla.onclik = function () {
-        tocaSom(idAudio);
- }
-
- tecla.onkeydown = function () {
-
-    if ('Espaço'){
-tecla.classList.add(ativa){}
+    const instrumento = tecla.classList[1];
+    const idAudio = '#som_${instrumento}'; //template string
+ 
+    tecla.onclick = function () {
+        tocaSomPom(idAudio);
     }
-    tecla.classList.add('ativa')
- }
 
- tecla.onkeyup = function () {
-    tecla.classList.remove('ativa')
- }
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'space' || evento.code === 'Enter'){
+        tecla.classList.add ('ativa');
+    }
 
 }
 
+tecla.onkeyup = function() {
+    tecla.classList.remove('ativa');
+}
